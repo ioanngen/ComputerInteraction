6645db682ec3bbd5c390941d8b0c1e93e1aea01d
SpirosSaridakis <spiros.sar1234@hotmail.com>
2023-07-12 21:15:04 +0300

    Made some changes to the main menu
    Added the traffic and weather indicators, also added the logo.

--- PersonalAssistant/MainMenu.Designer.cs
+++ PersonalAssistant/MainMenu.Designer.cs
@@ -28,11 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
@@ -44,11 +42,22 @@
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
-            this.tmrNews = new System.Windows.Forms.Timer(this.components);
-            this.imageListNews = new System.Windows.Forms.ImageList(this.components);
             this.pbNews = new System.Windows.Forms.PictureBox();
             this.LoadNews = new System.Windows.Forms.Button();
+            this.TrafficBox = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbNews)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrafficBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -73,15 +82,6 @@
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(651, 27);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(212, 29);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Weather and Traffic";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
@@ -207,17 +207,6 @@
             this.button14.TabIndex = 14;
             this.button14.UseVisualStyleBackColor = true;
             // 
-            // tmrNews
-            // 
-            this.tmrNews.Enabled = true;
-            this.tmrNews.Interval = 300;
-            // 
-            // imageListNews
-            // 
-            this.imageListNews.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
-            this.imageListNews.ImageSize = new System.Drawing.Size(16, 16);
-            this.imageListNews.TransparentColor = System.Drawing.Color.Transparent;
-            // 
             // pbNews
             // 
             this.pbNews.Image = ((System.Drawing.Image)(resources.GetObject("pbNews.Image")));
@@ -239,6 +228,89 @@
             this.LoadNews.UseVisualStyleBackColor = true;
             this.LoadNews.Click += new System.EventHandler(this.button13_Click);
             // 
+            // TrafficBox
+            // 
+            this.TrafficBox.Image = ((System.Drawing.Image)(resources.GetObject("TrafficBox.Image")));
+            this.TrafficBox.Location = new System.Drawing.Point(738, 45);
+            this.TrafficBox.Name = "TrafficBox";
+            this.TrafficBox.Size = new System.Drawing.Size(65, 74);
+            this.TrafficBox.TabIndex = 17;
+            this.TrafficBox.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(738, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(139, 20);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.Text = "Weather in: Athens";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(812, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 74);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(738, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(139, 20);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.Text = "Traffic in: Athens";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(773, 171);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(65, 74);
+            this.pictureBox3.TabIndex = 20;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(301, 87);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(208, 207);
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(515, 45);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(156, 140);
+            this.pictureBox4.TabIndex = 23;
+            this.pictureBox4.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(530, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(114, 20);
+            this.textBox4.TabIndex = 24;
+            this.textBox4.Text = "Welcome Spyros";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -246,6 +318,14 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(889, 501);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TrafficBox);
             this.Controls.Add(this.LoadNews);
             this.Controls.Add(this.pbNews);
             this.Controls.Add(this.button14);
@@ -259,12 +339,16 @@
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "MainMenu";
             this.Text = "Personal Assisant";
             ((System.ComponentModel.ISupportInitialize)(this.pbNews)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrafficBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -274,7 +358,6 @@
 
         private Button button1;
         private Button button2;
-        private Button button3;
         private Button button4;
         private Button button5;
         private Button button6;
@@ -286,9 +369,15 @@
         private Button button11;
         private Button button12;
         private Button button14;
-        private System.Windows.Forms.Timer tmrNews;
-        private ImageList imageListNews;
         private PictureBox pbNews;
         private Button LoadNews;
+        private PictureBox TrafficBox;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private TextBox textBox3;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private TextBox textBox4;
     }
 }
